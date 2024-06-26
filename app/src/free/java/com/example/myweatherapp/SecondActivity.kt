5f7ba27961158d7733c92b58ee54b6f9dd1b3170
Maria <157mperez@gmail.com>
2024-06-26 19:18:08 +0200

    añadir botón para volver atrás en free en secoind activity

--- app/src/free/java/com/example/myweatherapp/SecondActivity.kt
+++ app/src/free/java/com/example/myweatherapp/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myweatherapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,16 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val backButton = findViewById<View>(R.id.backButton)
+        backButton.setOnClickListener {
+            navigateBackToHome()
+        }
+    }
+
+    private fun navigateBackToHome() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
